af84138c6e6ad2bbb54b2df5256ee7f27a26abfb
Куликов Александр <akulikov@stc-spb.ru>
2020-11-15 23:37:14 +0300

    update content

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const host = '0.0.0.0';
 // приложение
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World Update');
 });
 
 app.listen(port, host);
